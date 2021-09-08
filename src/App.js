f02fb7cb4938f3d1efdc4e6f30bf6caaebd13746
Diego C <diegocs2410@gmail.com>
2021-09-07 22:57:21 -0500

    Plantilla React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1 className='text-center my-5 display-3'>Hello</h1>
         </div>
     );
 }
